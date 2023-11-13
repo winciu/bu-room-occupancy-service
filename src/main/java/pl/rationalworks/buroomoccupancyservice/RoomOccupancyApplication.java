9dddbe73fd2ce1b6d01d4be4c0a1cf7fe6b13716
winciu <winciu@gazeta.pl>
2023-11-13 20:49:58 +0100

    initial project setup

---
+++ src/main/java/pl/rationalworks/buroomoccupancyservice/RoomOccupancyApplication.java
@@ -0,0 +1,13 @@
+package pl.rationalworks.buroomoccupancyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rationalworks/buroomoccupancyservice/RoomOccupancyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rationalworks.buroomoccupancyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomOccupancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
