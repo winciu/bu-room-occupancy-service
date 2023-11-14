c1e1f234c751bdc3e6cd7f3a1b545e2e222a8978
winciu <winciu@gazeta.pl>
2023-11-14 19:59:28 +0100

    refactored tests (added utility methods for given/when/then parts)

--- src/test/java/pl/rationalworks/buroomoccupancyservice/api/HotelTests.java
+++ src/test/java/pl/rationalworks/buroomoccupancyservice/api/HotelTests.java
@@ -8,71 +8,92 @@ import static pl.rationalworks.buroomoccupancyservice.api.RoomType.PREMIUM;
 
 public class HotelTests {
 
+    private static Hotel createHotelWithAvailableRooms(RoomType[] roomTypes, Integer... availableRooms) {
+        Hotel hotel = new Hotel();
+        for (int i = 0; i < Math.min(roomTypes.length, availableRooms.length); i++) {
+            hotel.setAvailableRooms(roomTypes[i], availableRooms[i]);
+        }
+        return hotel;
+    }
+
+    private static void bookRooms(Hotel hotel, RoomType[] roomTypes, Integer... prices) {
+        for (int i = 0; i < Math.min(roomTypes.length, prices.length); i++) {
+            hotel.bookARoom(roomTypes[i], prices[i]);
+        }
+    }
+
+    private static RoomType[] roomTypes(RoomType... roomTypes) {
+        return roomTypes;
+    }
+
+    private static void assertAvailabilityOfRooms(Hotel hotel, RoomType[] roomTypes, int... availabilityOfRooms) {
+        for (int i = 0; i < Math.min(roomTypes.length, availabilityOfRooms.length); i++) {
+            assertEquals(availabilityOfRooms[i], hotel.getAvailableRooms(roomTypes[i]));
+        }
+    }
+
+    private static void assertNumberOfBookedRooms(Hotel hotel, RoomType[] roomTypes, int... roomsBooked) {
+        for (int i = 0; i < Math.min(roomTypes.length, roomsBooked.length); i++) {
+            assertEquals(roomsBooked[i], hotel.getNumberOfBookedRooms(roomTypes[i]));
+        }
+    }
+
+    private static void assertValueOfBookedRooms(Hotel hotel, RoomType[] roomTypes, int... roomPrices) {
+        for (int i = 0; i < Math.min(roomTypes.length, roomPrices.length); i++) {
+            assertEquals(roomPrices[i], hotel.getValueOfBookedRooms(roomTypes[i]));
+        }
+    }
+
     @Test
     void shouldSetRoomAvailabilityForAHotel() {
-        Hotel hotel = new Hotel();
-        hotel.setAvailableRooms(ECONOMY, 1);
-        hotel.setAvailableRooms(PREMIUM, 3);
+        Hotel hotel = createHotelWithAvailableRooms(roomTypes(ECONOMY, PREMIUM), 1, 3);
 
-        assertEquals(1, hotel.getAvailableRooms(ECONOMY));
-        assertEquals(3, hotel.getAvailableRooms(PREMIUM));
+        assertAvailabilityOfRooms(hotel, roomTypes(ECONOMY, PREMIUM), 1, 3);
     }
 
     @Test
     void byDefaultHotelHasNoRoomsOfAnyType() {
-        Hotel hotel = new Hotel();
-        assertEquals(0, hotel.getAvailableRooms(ECONOMY));
-        assertEquals(0, hotel.getAvailableRooms(PREMIUM));
+        Hotel hotel = createHotelWithAvailableRooms(roomTypes());
+
+        assertAvailabilityOfRooms(hotel, roomTypes(ECONOMY, PREMIUM), 0, 0);
     }
 
     @Test
     void clientCannotBookAnEconomyRoomPayingTooMuch() {
-        Hotel hotel = new Hotel();
-        hotel.setAvailableRooms(ECONOMY, 1);
-        hotel.bookARoom(ECONOMY, 100);
-        assertEquals(1, hotel.getAvailableRooms(ECONOMY));
+        Hotel hotel = createHotelWithAvailableRooms(roomTypes(ECONOMY), 1);
+        bookRooms(hotel, roomTypes(ECONOMY), 100);
+
+        assertAvailabilityOfRooms(hotel, roomTypes(ECONOMY), 1);
     }
 
     @Test
     void clientCanBookARoomOfAGivenType() {
-        Hotel hotel = new Hotel();
-        hotel.setAvailableRooms(ECONOMY, 1);
-        hotel.setAvailableRooms(PREMIUM,2);
-        hotel.bookARoom(PREMIUM, 30);
-        hotel.bookARoom(ECONOMY, 44);
+        Hotel hotel = createHotelWithAvailableRooms(roomTypes(ECONOMY, PREMIUM), 1, 2);
+        bookRooms(hotel, roomTypes(PREMIUM, ECONOMY), 30, 44);
 
-        assertEquals(1, hotel.getAvailableRooms(PREMIUM));
-        assertEquals(0, hotel.getAvailableRooms(ECONOMY));
-
-        assertEquals(1, hotel.getNumberOfBookedRooms(PREMIUM));
-        assertEquals(1, hotel.getNumberOfBookedRooms(ECONOMY));
-
-        assertEquals(30, hotel.getValueOfBookedRooms(PREMIUM));
-        assertEquals(44, hotel.getValueOfBookedRooms(ECONOMY));
+        assertAvailabilityOfRooms(hotel, roomTypes(ECONOMY, PREMIUM), 0, 1);
+        assertNumberOfBookedRooms(hotel, roomTypes(ECONOMY, PREMIUM), 1, 1);
+        assertValueOfBookedRooms(hotel, roomTypes(ECONOMY, PREMIUM), 44, 30);
     }
 
     @Test
     void afterBookingMoreThenOneRoomNumberAndValueOfBookedRoomsShouldMatch() {
-        Hotel hotel = new Hotel();
-        hotel.setAvailableRooms(ECONOMY, 2);
-        hotel.bookARoom(ECONOMY, 30);
-        hotel.bookARoom(ECONOMY, 44);
+        Hotel hotel = createHotelWithAvailableRooms(roomTypes(ECONOMY), 2);
+        bookRooms(hotel, roomTypes(ECONOMY, ECONOMY), 30, 44);
 
-        assertEquals(0, hotel.getAvailableRooms(ECONOMY));
-        assertEquals(2, hotel.getNumberOfBookedRooms(ECONOMY));
-        assertEquals(74, hotel.getValueOfBookedRooms(ECONOMY));
+        assertAvailabilityOfRooms(hotel, roomTypes(ECONOMY), 0);
+        assertNumberOfBookedRooms(hotel, roomTypes(ECONOMY), 2);
+        assertValueOfBookedRooms(hotel, roomTypes(ECONOMY), 74);
     }
 
     @Test
-    void userCannotBookMoreThanAvailableNumberOfRooms() {
-        Hotel hotel = new Hotel();
-        hotel.setAvailableRooms(ECONOMY, 1);
-        hotel.bookARoom(ECONOMY, 26);
-        hotel.bookARoom(ECONOMY, 44);
+    void itIsNotPossibleToBookMoreThanAvailableNumberOfRooms() {
+        Hotel hotel = createHotelWithAvailableRooms(roomTypes(ECONOMY), 1);
+        bookRooms(hotel, roomTypes(ECONOMY, ECONOMY), 26, 44);
 
-        assertEquals(0, hotel.getAvailableRooms(ECONOMY));
-        assertEquals(1, hotel.getNumberOfBookedRooms(ECONOMY));
-        assertEquals(26, hotel.getValueOfBookedRooms(ECONOMY));
+        assertAvailabilityOfRooms(hotel, roomTypes(ECONOMY), 0);
+        assertNumberOfBookedRooms(hotel, roomTypes(ECONOMY), 1);
+        assertValueOfBookedRooms(hotel, roomTypes(ECONOMY), 26);
     }
 
 }
