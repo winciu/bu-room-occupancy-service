4c1c80aa9b28d6b782eced50cfe94611fe2b047d
winciu <winciu@gazeta.pl>
2023-11-14 19:14:39 +0100

    implemented first version of booking algorithm added some tests to verify basic operations and logic

--- src/main/java/pl/rationalworks/buroomoccupancyservice/api/Hotel.java
+++ src/main/java/pl/rationalworks/buroomoccupancyservice/api/Hotel.java
@@ -2,12 +2,20 @@ package pl.rationalworks.buroomoccupancyservice.api;
 
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @NoArgsConstructor
 public class Hotel {
-    private Map<RoomType, Integer> availableRooms = new HashMap<>();
+    private final Map<RoomType, Integer> availableRooms = new HashMap<>();
+
+    /**
+     * A map storing an information about already booked rooms of a given type
+     * and a total price (after summing up each booking price in a given list).
+     */
+    private final Map<RoomType, List<Integer>> bookedRooms = new HashMap<>();
 
     public Integer setAvailableRooms(RoomType roomType, Integer amount){
         return availableRooms.put(roomType, amount);
@@ -15,5 +23,34 @@ public class Hotel {
 
     public Integer getAvailableRooms(RoomType roomType) {
         return availableRooms.getOrDefault(roomType, 0);
+    }
+
+    public Integer getNumberOfBookedRooms(RoomType roomType) {
+        return bookedRooms.get(roomType).size();
+    }
+
+    public Integer getValueOfBookedRooms(RoomType roomType) {
+        return bookedRooms.get(roomType).stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public void bookARoom(RoomType roomType, Integer price) {
+        availableRooms.computeIfPresent(roomType, (rt, amountOfAvailableRooms) -> {
+            bookedRooms.putIfAbsent(rt, new ArrayList<>());
+            //if  we still have available rooms
+            boolean tooMuchForEconomy = roomType.equals(RoomType.ECONOMY) && price >= 100;
+            if (amountOfAvailableRooms > 0) {
+                bookedRooms.computeIfPresent(roomType, (rType, prices) -> {
+                    if (!tooMuchForEconomy) {
+                        prices.add(price);
+                    }
+                    return prices;
+                });
+                if (tooMuchForEconomy) {
+                    return amountOfAvailableRooms;
+                }
+                return amountOfAvailableRooms - 1;
+            }
+            return amountOfAvailableRooms;
+        });
     }
 }
--- src/test/java/pl/rationalworks/buroomoccupancyservice/api/HotelTests.java
+++ src/test/java/pl/rationalworks/buroomoccupancyservice/api/HotelTests.java
@@ -24,4 +24,55 @@ public class HotelTests {
         assertEquals(0, hotel.getAvailableRooms(ECONOMY));
         assertEquals(0, hotel.getAvailableRooms(PREMIUM));
     }
+
+    @Test
+    void clientCannotBookAnEconomyRoomPayingTooMuch() {
+        Hotel hotel = new Hotel();
+        hotel.setAvailableRooms(ECONOMY, 1);
+        hotel.bookARoom(ECONOMY, 100);
+        assertEquals(1, hotel.getAvailableRooms(ECONOMY));
+    }
+
+    @Test
+    void clientCanBookARoomOfAGivenType() {
+        Hotel hotel = new Hotel();
+        hotel.setAvailableRooms(ECONOMY, 1);
+        hotel.setAvailableRooms(PREMIUM,2);
+        hotel.bookARoom(PREMIUM, 30);
+        hotel.bookARoom(ECONOMY, 44);
+
+        assertEquals(1, hotel.getAvailableRooms(PREMIUM));
+        assertEquals(0, hotel.getAvailableRooms(ECONOMY));
+
+        assertEquals(1, hotel.getNumberOfBookedRooms(PREMIUM));
+        assertEquals(1, hotel.getNumberOfBookedRooms(ECONOMY));
+
+        assertEquals(30, hotel.getValueOfBookedRooms(PREMIUM));
+        assertEquals(44, hotel.getValueOfBookedRooms(ECONOMY));
+    }
+
+    @Test
+    void afterBookingMoreThenOneRoomNumberAndValueOfBookedRoomsShouldMatch() {
+        Hotel hotel = new Hotel();
+        hotel.setAvailableRooms(ECONOMY, 2);
+        hotel.bookARoom(ECONOMY, 30);
+        hotel.bookARoom(ECONOMY, 44);
+
+        assertEquals(0, hotel.getAvailableRooms(ECONOMY));
+        assertEquals(2, hotel.getNumberOfBookedRooms(ECONOMY));
+        assertEquals(74, hotel.getValueOfBookedRooms(ECONOMY));
+    }
+
+    @Test
+    void userCannotBookMoreThanAvailableNumberOfRooms() {
+        Hotel hotel = new Hotel();
+        hotel.setAvailableRooms(ECONOMY, 1);
+        hotel.bookARoom(ECONOMY, 26);
+        hotel.bookARoom(ECONOMY, 44);
+
+        assertEquals(0, hotel.getAvailableRooms(ECONOMY));
+        assertEquals(1, hotel.getNumberOfBookedRooms(ECONOMY));
+        assertEquals(26, hotel.getValueOfBookedRooms(ECONOMY));
+    }
+
 }
